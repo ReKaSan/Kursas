85e3eff7308f64d8e50121993bc76caba3bf9399
ReKaSan <redaskardauskas@gmail.com>
2019-08-04 02:08:56 +0300

    js4 darbas

---
+++ Namu darbai/JS4/js/custom.js
@@ -0,0 +1,9 @@
+function arJums18 (x)
+{
+    if (x >= 18) {
+        document.write ("Jus galite ieiti i svetaine.")
+    }
+    else {
+        document.write ("Jus neglite ieiti i svetaine.")
+    }
+}
