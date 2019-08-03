cc18bd3eeeadfc2649e1d26571dc57ee736474c9
ReKaSan <redaskardauskas@gmail.com>
2019-08-04 02:07:50 +0300

    js3 darbas

---
+++ Namu darbai/JS3/js/custom.js
@@ -0,0 +1,13 @@
+//function dalybaDu (x, y)
+//{
+//
+//    let result = x / y;
+//    return result;
+//}
+
+function dalybaDu (x, y)
+{
+
+    let result = 12 / 6;
+    document.write (result);
+}
