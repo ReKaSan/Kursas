b5c015777a4932ef0f8ba348bd0ccfb8c2aa950a
ReKaSan <redaskardauskas@gmail.com>
2019-08-04 23:27:14 +0300

    js4 pataisytas

--- Namu darbai/JS4/js/custom.js
+++ Namu darbai/JS4/js/custom.js
@@ -1,9 +1,9 @@
-function arJums18 (x)
+function arJums18(x)
 {
     if (x >= 18) {
-        document.write ("Jus galite ieiti i svetaine.")
+        document.write("Jus galite ieiti i svetaine.")
     }
     else {
-        document.write ("Jus neglite ieiti i svetaine.")
+        document.write("Jus neglite ieiti i svetaine.")
     }
 }
