8d812acec0c8607fad1c8fb66fad189dba78ec18
ReKaSan <redaskardauskas@gmail.com>
2019-08-05 20:32:46 +0300

    js5 pataisytas

--- Namu darbai/JS5/js/custom.js
+++ Namu darbai/JS5/js/custom.js
@@ -1,11 +1,24 @@
-function arJums18(amzius)
-{
+//function arJums18(amzius)
+//{
+//    if (amzius >= 18) {
+//    result = Boolean(amzius);
+//    return result;
+//    }
+//    else {
+//    result = false;
+//    return result;
+//    }
+//}
+
+function arPilnametis(amzius){
     if (amzius >= 18) {
-    result = Boolean(amzius);
-    return result;
+        return true;
     }
     else {
-    result = false;
-    return result;
+        return false;
     }
+}
+
+function arPilnametis2(amzius){
+    return amzius >= 18;
 }
