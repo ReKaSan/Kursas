55d61c0c3a4c2016e099a19eb4a792cb657f22c3
ReKaSan <redaskardauskas@gmail.com>
2019-08-06 18:58:36 +0300

    js9 namu darbas

---
+++ Namu darbai/JS9/js/custom.js
@@ -0,0 +1,8 @@
+function spausdintiVardusPavardes(vardai)
+{
+document.write("<ol>");
+ for (let i = 0; i < vardai.length; i++){
+        document.write("<li>" + vardai[i] + "</li>");
+    }
+    document.write("</ol>");
+}
