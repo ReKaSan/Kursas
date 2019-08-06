83ee22a658086ba7817991f8fb7c53a9ab2310d3
ReKaSan <redaskardauskas@gmail.com>
2019-08-07 01:50:56 +0300

    js8 namu darbas

---
+++ Namu darbai/JS8/js/custom.js
@@ -0,0 +1,14 @@
+function duokManSavaitesDienaPagalSkaiciu(index){
+    let savaitesDienos = [
+        "pirmadienis",
+        "antradienis",
+        "treciadienis",
+        "ketvirtadienis",
+        "penktadienis",
+        "sestadienis",
+        "sekmadienis"
+    ];
+    let savaite = savaitesDienos.indexOf[];
+    return index;
+    
+}
