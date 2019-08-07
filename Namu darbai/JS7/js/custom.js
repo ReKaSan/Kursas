e72cd542d1d62675dcf60816c26cdd63ad79973c
ReKaSan <redaskardauskas@gmail.com>
2019-08-07 17:34:42 +0300

    js7 pataisytas

---
+++ Namu darbai/JS7/js/custom.js
@@ -0,0 +1,8 @@
+function vardasSpausdinamasXKartu() {
+    let kartas = prompt("Kiek kartu norite spausdinti varda?");
+    let x = 0;
+    while (x < kartas) {
+        x++;
+        document.write("Redas" + "<br>");
+    }
+}
